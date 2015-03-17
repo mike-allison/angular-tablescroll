e868b8d50aeec1c9add25dee343ea92a7002ea36
Mike Allison <mike.allison@agilixideaworks.com>
2015-03-17 11:16:03 -0500

    Add npm deps, gulp build and nuget

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp'),
+    nuget = require('gulp-nuget'),
+    tsc = require('gulp-tsc'),
+    clean = require('gulp-clean'),
+    runSequence = require('run-sequence'),
+    project = require('./package.json'),
+    nugetApi = require('../nugetApi.json'),
+    nugetPath = '../nuget.exe';
+
+gulp.task('nuget-pack', function() {
+    return gulp.src('./dist/' + project.name + '.' + '.js' )
+        .pipe(nuget.pack({
+            nuspec: 'project.nuspec',
+            nuget: nugetPath,
+            version: project.version,
+            workingDirectory: '.tmp/'
+        }))
+        .pipe(gulp.dest('nuget/'));
+});
+
+gulp.task('nuget-push', function() {
+    return gulp.src('nuget/' + project.name + '.' + project.version + '.nupkg')
+        .pipe(nuget.push({
+            feed: 'https://www.nuget.org/',
+            nuget: nugetPath,
+            apiKey: nugetApi.key
+        }));
+});
+
+gulp.task('dist', function() {
+    return gulp.src([project.name + '.ts'])
+        .pipe(tsc({out: project.name + '.js', removeComments: false}))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src(['./*.nupk','./dist/**','.tmp/**']).pipe(clean());
+});
+
+gulp.task('clean-leftovers', function() {
+    return gulp.src(['./*.nupkg']).pipe(clean());
+})
+
+gulp.task('nuget', function() {
+    runSequence('clean','dist','nuget-pack','clean-leftovers');
+});
